00eec51862d512cd7a1b846463e2b55438a837e5
thomasmeston <thomasmeston@users.noreply.github.com>
2026-07-25 22:35:33 +0000

    deploy: cb80ddf5ea86382274519ec8e5b49d7d4a1ba686

--- src/assets.js
+++
@@ -1,8 +0,0 @@
-// Define texture paths
-export const images = {
-    woodTexture: 'src/assets/images/wood_texture.png',
-    notebookPaper: 'src/assets/images/notebook_texture.png'
-};
-
-// Debug log to verify the paths
-console.log('Texture paths:', images); 
